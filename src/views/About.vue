c7267a57f3b35d3a548b0e4b721b3623fa78d3da
Fahad <riyad9949@gmail.com>
2021-10-16 14:18:43 +0600

    update about page and add meta seo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This game is made by Fahad.</h1>
+    <h1>Play the game with your friend or computer!</h1>
+    <div style="position: fixed; bottom: 3px; text-align:center;">
+      <h3 class="d-block m-auto">&copy;Copyright 2021-2022 | This game is made by Fahad.</h3>
+    </div>
   </div>
 </template>
